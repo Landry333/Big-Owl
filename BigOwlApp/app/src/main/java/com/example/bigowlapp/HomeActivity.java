957cfbbf523b77aff433d1934cffedbfced69d97
The Phi Nguyen <Thephi.nguyen1994@gmail.com>
2020-09-25 13:59:50 -0400

    Initial commit

---
+++ BigOwlApp/app/src/main/java/com/example/bigowlapp/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.example.bigowlapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.PrivateKey;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth m_FirebaseAuth;
+    private FirebaseAuth.AuthStateListener m_AuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogOut = findViewById(R.id.Logout);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ BigOwlApp/app/src/main/java/com/example/bigowlapp/LoginActivity.java
@@ -0,0 +1,115 @@
+package com.example.bigowlapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    public EditText emailId, password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth m_FirebaseAuth;
+    private FirebaseAuth.AuthStateListener m_AuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        m_FirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        btnSignIn = findViewById(R.id.button);
+        tvSignUp = findViewById(R.id.textView);
+
+        m_AuthStateListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser m_FirebaseUser = m_FirebaseAuth.getCurrentUser();
+                if(m_FirebaseUser != null)
+                {
+                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Please login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pass = password.getText().toString();
+
+                if(email.isEmpty())
+                {
+                    emailId.setError("Please enter a valid email");
+                    emailId.requestFocus();
+                }
+                else if(pass.isEmpty())
+                {
+                    password.setError("Please enter your password");
+                    emailId.requestFocus();
+                }
+                else if(email.isEmpty() && pass.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this, "Fields are empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && pass.isEmpty()))
+                {
+                    m_FirebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful())
+                            {
+                                Toast.makeText(LoginActivity.this, "Login error, Please login again", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else
+                            {
+                                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(i);
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "An error has occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        m_FirebaseAuth.addAuthStateListener(m_AuthStateListener);
+    }
+}
