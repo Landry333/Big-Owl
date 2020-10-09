004400ac52a6da55b8dc15a0f5f49b7b21f1f55f
Shawn <ShawnGuerraBautista@gmail.com>
2020-10-08 21:57:53 -0400

    [#58] Did one method for repository
    Co-authored-by: RefatK <refat-17@hotmail.com>

--- BigOwlApp/app/src/main/java/repository/UserRepository.java
+++ BigOwlApp/app/src/main/java/repository/UserRepository.java
@@ -1,6 +1,12 @@
 package repository;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import database.Firestore;
 import model.User;
@@ -13,8 +19,25 @@ public class UserRepository {
         mFirebaseFirestore = Firestore.getDatabase();
     }
 
-    public User getUserByPhoneNumber(String phoneNumber) {
-        return mFirebaseFirestore.collection("users").document(phoneNumber);
+    // TODO: Solve Asynchronous problem
+    public MutableLiveData<User> getUserByPhoneNumber(String phoneNumber) {
+        MutableLiveData<User> userData = new MutableLiveData<>();
+        mFirebaseFirestore.collection("users")
+                .whereEqualTo("phoneNumber", phoneNumber)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            User u = task.getResult().getDocuments().get(0).toObject(User.class);
+                            MutableLiveData<User> userData = new MutableLiveData<>();
+                            userData.setValue(u);
+                        }else{
+                            // TODO: Log the error
+                        }
+                    }
+                });
+        return userData;
     }
 
 }
