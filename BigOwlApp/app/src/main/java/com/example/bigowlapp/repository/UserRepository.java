cb0960f40067f50630babc0a60caad1088307a0a
Shawn <ShawnGuerraBautista@gmail.com>
2020-10-10 19:55:19 -0400

    [#58] Added getByUserId in repository
    Co-authored-by: RefatK <refat-17@hotmail.com>

--- BigOwlApp/app/src/main/java/com/example/bigowlapp/repository/UserRepository.java
+++ BigOwlApp/app/src/main/java/com/example/bigowlapp/repository/UserRepository.java
@@ -9,31 +9,49 @@ import com.example.bigowlapp.model.User;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import static android.content.ContentValues.TAG;
-
 public class UserRepository {
 
     private FirebaseFirestore mFirebaseFirestore;
     private CollectionReference usersRef;
 
     public UserRepository() {
-            mFirebaseFirestore = Firestore.getDatabase();
-            usersRef = mFirebaseFirestore.collection("users");
+        mFirebaseFirestore = Firestore.getDatabase();
+        usersRef = mFirebaseFirestore.collection("users");
     }
+
+    public MutableLiveData<User> getUserById(String id) {
+        MutableLiveData<User> userData = new MutableLiveData<>();
+        usersRef.document(id)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        User user = task.getResult().toObject(User.class);
+                        userData.setValue(user);
+                    } else {
+                        Log.e(getClassName(), "Error getting documents: ", task.getException());
+                    }
+                });
+        return userData;
+    }
+
 
     public MutableLiveData<User> getUserByPhoneNumber(String phoneNumber) {
         MutableLiveData<User> userData = new MutableLiveData<>();
         usersRef.whereEqualTo("phoneNumber", phoneNumber)
                 .limit(1)
                 .get()
-                .addOnCompleteListener(userTask -> {
-                        if (userTask.isSuccessful()) {
-                            User user = userTask.getResult().getDocuments().get(0).toObject(User.class);
-                            userData.setValue(user);
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", userTask.getException());
-                        }
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        User user = task.getResult().getDocuments().get(0).toObject(User.class);
+                        userData.setValue(user);
+                    } else {
+                        Log.e(getClassName(), "Error getting documents: ", task.getException());
+                    }
                 });
         return userData;
+    }
+
+    private String getClassName() {
+        return this.getClass().toString();
     }
 }
