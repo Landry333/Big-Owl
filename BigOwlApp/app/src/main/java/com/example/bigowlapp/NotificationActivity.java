905c364cde1ad3489880e3326ffc15b85c425701
ggebran <ggebran95@gmail.com>
2020-10-18 14:59:20 -0400

    [#52]Added NotificationActivity and NotificationAdapter

---
+++ BigOwlApp/app/src/main/java/com/example/bigowlapp/NotificationActivity.java
@@ -0,0 +1,55 @@
+package com.example.bigowlapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.example.bigowlapp.model.Notification;
+import com.example.bigowlapp.repository.NotificationRepository;
+import com.example.bigowlapp.repository.Repository;
+import com.example.bigowlapp.utils.NotificationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private NotificationRepository notificationRepository;
+    private LiveData<List<Notification>> notificationListData;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        recyclerView = findViewById(R.id.recyclerview_notifications);
+
+        notificationRepository = new NotificationRepository();
+        notificationListData = notificationRepository.getAllDocumentsFromCollection(Notification.class);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        notificationListData.observe(this, notifications -> {
+            ArrayList<String> type = new ArrayList<>();
+            for(Notification n : notifications){
+                type.add(n.getType());
+            }
+
+            mAdapter = new NotificationAdapter(type , this);
+        });
+
+        recyclerView.setAdapter(mAdapter);
+
+    }
+}
---
+++ BigOwlApp/app/src/main/java/com/example/bigowlapp/utils/NotificationAdapter.java
@@ -0,0 +1,62 @@
+package com.example.bigowlapp.utils;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bigowlapp.R;
+import com.example.bigowlapp.model.Notification;
+
+import java.util.ArrayList;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+
+    private static final String TAG = "NotificationAdapter";
+    private ArrayList<String> mNotificationTitles = new ArrayList<>();
+    private Context mContext;
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView)LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        holder.textView.setText(mNotificationTitles.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotificationTitles.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textView;
+        public ConstraintLayout parentLayout;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            textView = v.findViewById(R.id.notification_name);
+            parentLayout = v.findViewById(R.id.parent_layout);
+        }
+    }
+
+    public NotificationAdapter(ArrayList<String> notificationTitles, Context context) {
+        mNotificationTitles = notificationTitles;
+        mContext = context;
+    }
+}
