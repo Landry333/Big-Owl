c855854c94e89cf7589acf13e693a336a19bccf6
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2020-10-18 21:30:10 -0400

    #43 Added pop-up for permission

--- app/src/main/java/com/example/bigowlapp/HomeActivity.java
+++ app/src/main/java/com/example/bigowlapp/HomeActivity.java
@@ -1,15 +1,23 @@
 package com.example.bigowlapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.bigowlapp.model.User;
 import com.example.bigowlapp.repository.UserRepository;
@@ -20,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
     Button btnLogOut, sendSmsInvitation, btnSearchUsers, btnMonitoringGroup, btnSupervisedGroup, btnMonitoringList;
     FirebaseAuth m_FirebaseAuth;
     private FirebaseAuth.AuthStateListener m_AuthStateListener;
+    private int CONTACT_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +71,7 @@ public class HomeActivity extends AppCompatActivity {
             btnMonitoringGroup.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent i = new Intent(HomeActivity.this, MonitoringGroupListActivity.class);
+                    Intent i = new Intent(HomeActivity.this, MonitoringGroupPageActivity.class);
                     startActivity(i);
                 }
             });
@@ -77,21 +86,61 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
 
-            btnSearchUsers = findViewById(R.id.btnSearchUsers);
+            btnSearchUsers = findViewById(R.id.btnPhoneContacts);
 
             btnSearchUsers.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    if(ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(HomeActivity.this, "This permission is already granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        requestContactPermission();
+
+                    }
 
                     Intent i = new Intent(HomeActivity.this, SearchContactsToSupervise.class);
                     startActivity(i);
                 }
             });
-
         }
         catch (Exception ex)
         {
 
+        }
+    }
+
+    private void requestContactPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("Permission is required to read phone Contacts")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            ActivityCompat.requestPermissions(HomeActivity.this, new String[] {Manifest.permission.READ_CONTACTS},CONTACT_PERMISSION_CODE );
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS},CONTACT_PERMISSION_CODE );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CONTACT_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
--- app/src/main/java/com/example/bigowlapp/MonitoringGroupPageActivity.java
+++ app/src/main/java/com/example/bigowlapp/MonitoringGroupPageActivity.java
@@ -1,37 +1,58 @@
 package com.example.bigowlapp;
 
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
+import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class MonitoringGroupPageActivity extends AppCompatActivity {
+    private ListView users_listview;
     private TextView groupName, supervisorName;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private String supervisorFullName;
+    private List<User> mUsers, mUsersShow;
+    private List<String> mSupervisedGroup;
+
+    EditText search_users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monitoring_group_page);
+        setContentView(R.layout.activity_monitoring_group_list);
         initialize();
     }
 
     protected void initialize() {
+        mUsers = new ArrayList<>();
+        mSupervisedGroup = new ArrayList<>();
+
         try {
             FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
             if (currentUser != null) {
+
                 db.collection("groups")
                         .whereEqualTo("monitoringUserId", currentUser.getUid())
                         .get()
@@ -40,13 +61,48 @@ public class MonitoringGroupPageActivity extends AppCompatActivity {
                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                 if (task.isSuccessful()) {
                                     for (QueryDocumentSnapshot qds : task.getResult()) {
+
+                                        Group g = qds.toObject(Group.class);
                                         groupName = findViewById(R.id.textView_groupName);
                                         groupName.setText(qds.getString("name"));
 
-                                        supervisorName = findViewById(R.id.textView_supervisorName);
-                                        setSupervisorFullName(qds);
-                                        supervisorName.setText(supervisorFullName);
+
+                                        for (String supervisedUser : g.getSupervisedUserId()) {
+                                            mSupervisedGroup.add(supervisedUser);
+                                        }
+                                        //find Supervisor full name
+                                        db.collection("users").document(qds.getString("monitoringUserId")).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    User u = task.getResult().toObject(User.class);
+                                                    supervisorName = findViewById(R.id.textView_supervisorName);
+                                                    supervisorName.setText(u.toString());
+                                                }
+                                            }
+                                        });
                                     }
+
+                                    //List of all user to compare with
+                                    db.collection("users")
+                                            .whereIn(FieldPath.documentId(), mSupervisedGroup)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+
+                                                        for (QueryDocumentSnapshot qds : task.getResult()) {
+                                                            User u = qds.toObject(User.class);
+                                                            mUsers.add(u);
+                                                        }
+                                                        mUsersShow = mUsers;
+                                                        ArrayAdapter<User> arrayAdapter = new ArrayAdapter<User>(getBaseContext(), android.R.layout.simple_list_item_1, mUsersShow);
+                                                        users_listview = findViewById(R.id.list_view);
+                                                        users_listview.setAdapter((arrayAdapter));
+                                                    }
+                                                }
+                                            });
                                 }
                             }
                         });
@@ -54,18 +110,40 @@ public class MonitoringGroupPageActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
-    private void setSupervisorFullName(final QueryDocumentSnapshot qds) {
-        DocumentReference docRef = db.collection("users").document(qds.getString("monitoringUserId"));
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        search_users = findViewById(R.id.search_users);
+        search_users.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot dSS = task.getResult();
-                    supervisorFullName = (dSS.getString("firstName") + dSS.getString("lastName"));
-                }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //Log.d("test", "welp" + mUsers.size());
+                searchUsers(charSequence.toString().toLowerCase());
+                ArrayAdapter<User> arrayAdapter = new ArrayAdapter<User>(getBaseContext(), android.R.layout.simple_list_item_1, mUsersShow);
+                users_listview.setAdapter((arrayAdapter));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
+    }
+
+    private void searchUsers(String s) {
+/*
+        if(s == null || s.equals(""))
+        {
+            mUsersShow = mUsers;
+            return;
+        }*/
+        List<User> filteredUsers = mUsers.stream().filter(u -> {
+            boolean containInFirstName = u.getFirstName().toLowerCase().contains(s);
+            boolean containInLastName = u.getLastName().toLowerCase().contains(s);
+            return (containInFirstName || containInLastName);
+        }).collect(Collectors.toList());
+
+        mUsersShow = filteredUsers;
     }
 }
