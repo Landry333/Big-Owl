efe11abae8df04a849eacb4f63073ccd97b3c606
The Phi Nguyen <thephi.nguyen1994@gmail.com>
2020-10-30 13:02:23 -0400

    Issue [#106]
    Initial commit to upstream repo.

---
+++ app/src/main/java/com/example/bigowlapp/repository/ViewUserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bigowlapp.repository;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ViewUserRepository {
+}
---
+++ app/src/main/java/com/example/bigowlapp/viewModels/ViewUserViewModel.java
@@ -0,0 +1,6 @@
+package com.example.bigowlapp.viewModels;
+
+import com.example.bigowlapp.repository.ViewUserRepository;
+
+public class ViewUserViewModel {
+}
