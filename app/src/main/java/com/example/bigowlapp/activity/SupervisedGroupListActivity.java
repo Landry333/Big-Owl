79ed57c66fef662d23094fd2863baa3593b79924
Shawn <ShawnGuerraBautista@gmail.com>
2020-11-02 20:28:37 -0500

    [#105] Added the ViewModel implementation to the SupervisedGroupActivity

--- app/src/main/java/com/example/bigowlapp/activity/SupervisedGroupListActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/SupervisedGroupListActivity.java
@@ -2,30 +2,27 @@ package com.example.bigowlapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
 import com.example.bigowlapp.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.viewModel.SupervisedGroupListViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SupervisedGroupListActivity extends AppCompatActivity {
-    private static ArrayList<QueryDocumentSnapshot> qds = new ArrayList<QueryDocumentSnapshot>();
     private ListView lv;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private SupervisedGroupListViewModel supervisedGroupListViewModel;
+    private LiveData<List<Group>> listOfGroupsLiveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +35,18 @@ public class SupervisedGroupListActivity extends AppCompatActivity {
         try {
             FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
             if (currentUser != null) {
-                Query query = db.collection("groups").whereArrayContains("supervisedUserId", currentUser.getUid());
-                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            if (!qds.isEmpty())
-                                qds.clear();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                qds.add(document);
-                            }
-                            ArrayAdapter<QueryDocumentSnapshot> arrayAdapter = new ArrayAdapter<QueryDocumentSnapshot>(getBaseContext(), android.R.layout.simple_list_item_1, qds);
-                            lv = findViewById(R.id.listView_supervisedGroup);
-                            lv.setAdapter(arrayAdapter);
+                supervisedGroupListViewModel = new SupervisedGroupListViewModel();
+                listOfGroupsLiveData = supervisedGroupListViewModel.setListOfDocumentByArrayContains();
 
-                            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                // argument position gives the index of item which is clicked
-                                public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
-                                    Intent intent = new Intent(getBaseContext(), SupervisedGroupPageActivity.class);
-                                    startActivity(intent);
-                                }
-                            });
-                        }
-                    }
+                listOfGroupsLiveData.observe(this, groups -> {
+                    ArrayAdapter<Group> arrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_list_item_1, groups);
+                    lv = findViewById(R.id.listView_supervisedGroup);
+                    lv.setAdapter(arrayAdapter);
+                    // argument position gives the index of item which is clicked
+                    lv.setOnItemClickListener((arg0, v, position, arg3) -> {
+                        Intent intent = new Intent(getBaseContext(), SupervisedGroupPageActivity.class);
+                        startActivity(intent);
+                    });
                 });
             }
         } catch (Exception e) {
--- app/src/main/java/com/example/bigowlapp/viewModel/SupervisedGroupListViewModel.java
+++ app/src/main/java/com/example/bigowlapp/viewModel/SupervisedGroupListViewModel.java
@@ -1,12 +1,29 @@
 package com.example.bigowlapp.viewModel;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.repository.AuthRepository;
 import com.example.bigowlapp.repository.GroupRepository;
+
+import java.util.List;
 
 public class SupervisedGroupListViewModel {
 
+    private AuthRepository authRepository;
     private GroupRepository groupRepository;
+    private MutableLiveData<List<Group>> groupLiveData;
 
-    public SupervisedGroupListViewModel(){
+    public SupervisedGroupListViewModel() {
+        authRepository = new AuthRepository();
         groupRepository = new GroupRepository();
     }
+
+    public MutableLiveData<List<Group>> setListOfDocumentByArrayContains() {
+        groupLiveData = groupRepository.getListOfDocumentByArrayContains("supervisedUserId",
+                authRepository.getCurrentUser().getUid(), Group.class);
+        return groupLiveData;
+    }
+
+
 }
