85a2c74680b2e45f72a6c139e93e42f3bb68b6ce
Refat Kamal <refat.k17@gmail.com>
2020-11-02 21:12:22 -0500

    #104 added viewModel for MonitoringGroupPageActivity

---
+++ app/src/main/java/com/example/bigowlapp/viewModel/MonitoringGroupPageViewModel.java
@@ -0,0 +1,55 @@
+package com.example.bigowlapp.viewModel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.model.User;
+import com.example.bigowlapp.repository.AuthRepository;
+import com.example.bigowlapp.repository.GroupRepository;
+import com.example.bigowlapp.repository.UserRepository;
+
+import java.util.List;
+
+public class MonitoringGroupPageViewModel extends ViewModel {
+
+    private final AuthRepository authRepository;
+    private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
+
+    private MutableLiveData<Group> selectedGroup;
+    private MutableLiveData<List<User>> usersInGroup;
+
+    public MonitoringGroupPageViewModel() {
+        authRepository = new AuthRepository();
+        groupRepository = new GroupRepository();
+        userRepository = new UserRepository();
+    }
+
+    public LiveData<Group> getGroup() {
+        if (selectedGroup == null) {
+            selectedGroup = new MutableLiveData<>();
+            getAllUsersInGroup(selectedGroup.getValue().getuId());
+        }
+        return selectedGroup;
+    }
+
+    public LiveData<List<User>> getUsers() {
+        if (usersInGroup == null) {
+            usersInGroup = new MutableLiveData<>();
+            getAllUsersInGroup(selectedGroup.getValue().getuId());
+        }
+        return usersInGroup;
+    }
+
+    private void loadGroup() {
+        String userId = authRepository.getCurrentUser().getUid();
+        selectedGroup = groupRepository.getDocumentByAttribute("monitoringUserId", userId, Group.class);
+    }
+
+    public void getAllUsersInGroup(String groupId) {
+
+    }
+
+}
