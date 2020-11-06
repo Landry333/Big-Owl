24ed71143f2b8c4eaa930d9df1a1d23212e516b8
Landry <david92d92@gmail.com>
2020-11-06 01:15:05 -0500

    #136 fixing code style

--- app/src/androidTest/java/com/example/bigowlapp/activity/InvitationConfirmationActivityTest.java
+++ app/src/androidTest/java/com/example/bigowlapp/activity/InvitationConfirmationActivityTest.java
@@ -5,6 +5,7 @@ import android.app.Instrumentation;
 
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.bigowlapp.R;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,14 +16,13 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.junit.Assert.*;
-import com.example.bigowlapp.R;
+import static org.junit.Assert.assertNotNull;
 
 public class InvitationConfirmationActivityTest {
 
     @Rule
     public ActivityTestRule<InvitationConfirmationActivity> invitationConfirmationActivityTestRule = new ActivityTestRule<InvitationConfirmationActivity>(InvitationConfirmationActivity.class);
-    private InvitationConfirmationActivity invitationConfirmActivity =null;
+    private InvitationConfirmationActivity invitationConfirmActivity = null;
     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(HomePageActivity.class.getName(), null, false);
 
     @Before
@@ -32,7 +32,7 @@ public class InvitationConfirmationActivityTest {
     }
 
     @Test
-    public void launchHomePageActivityOnButtonClick(){
+    public void launchHomePageActivityOnButtonClick() {
         assertNotNull(invitationConfirmActivity.findViewById(R.id.returnHome));
         onView(withId(R.id.returnHome)).perform(click());
         Activity homePageActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 6000);
@@ -42,7 +42,7 @@ public class InvitationConfirmationActivityTest {
 
     @After
     public void tearDown() throws Exception {
-        invitationConfirmActivity=null;
+        invitationConfirmActivity = null;
     }
 
 }
