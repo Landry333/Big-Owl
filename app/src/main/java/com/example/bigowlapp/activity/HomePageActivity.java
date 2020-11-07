23685a107b9f402d4c3e9f2109bcc9a9b00495db
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2020-11-06 20:45:45 -0500

    #43 Added search by Phone number feature
    #43
    - Added extra feature to search by phone number
    
    - Feature is a bit rough right now but will be improved in the future
    
    * Currently need to match exactly the phone in the database *

--- app/src/main/java/com/example/bigowlapp/activity/HomePageActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/HomePageActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePageActivity extends AppCompatActivity {
     private final int CONTACT_PERMISSION_CODE = 1;
-    Button btnLogOut, sendSmsInvitation, btnSearchUsers, btnMonitoringGroup, btnSupervisedGroup, btnMonitoringList;
+    Button btnLogOut, sendSmsInvitation, btnAddUsers, btnMonitoringGroup, btnSupervisedGroup;
     FirebaseAuth m_FirebaseAuth;
     private FirebaseAuth.AuthStateListener m_AuthStateListener;
 
@@ -80,60 +80,17 @@ public class HomePageActivity extends AppCompatActivity {
                 }
             });
 
-            btnSearchUsers = findViewById(R.id.btnPhoneContacts);
+            btnAddUsers = findViewById(R.id.btnAddUsers);
 
-            btnSearchUsers.setOnClickListener(new View.OnClickListener() {
+            btnAddUsers.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (ContextCompat.checkSelfPermission(HomePageActivity.this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-                        //Toast.makeText(HomePageActivity.this, "This permission is already granted", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(HomePageActivity.this, SearchContactsToSupervise.class);
-                        startActivity(i);
-                    } else {
-                        requestContactPermission();
-                    }
+                    Intent i = new Intent(HomePageActivity.this, AddUsers.class);
+                    startActivity(i);
                 }
             });
         } catch (Exception ex) {
 
-        }
-    }
-
-    //TODO refactor to another class file
-    private void requestContactPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Permission needed")
-                    .setMessage("Permission is required to read phone Contacts")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int which) {
-                            ActivityCompat.requestPermissions(HomePageActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, CONTACT_PERMISSION_CODE);
-                        }
-                    })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .create().show();
-
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, CONTACT_PERMISSION_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == CONTACT_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(HomePageActivity.this, SearchContactsToSupervise.class);
-                startActivity(i);
-            } else {
-                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
-            }
         }
     }
 }
