1b810845fe8e4d8c369f010a67ef0c6f1a6ddff2
KZ <ng_khang_zheng@hotmail.com>
2020-11-08 06:05:02 -0500

    [#57] Unit test for editProfileActivity

--- app/src/main/java/com/example/bigowlapp/viewModel/EditProfileViewModel.java
+++ app/src/main/java/com/example/bigowlapp/viewModel/EditProfileViewModel.java
@@ -4,6 +4,7 @@ import com.example.bigowlapp.model.User;
 import com.example.bigowlapp.repository.AuthRepository;
 import com.example.bigowlapp.repository.UserRepository;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -12,7 +13,7 @@ public class EditProfileViewModel extends ViewModel {
 
     private final AuthRepository authRepository;
     private final UserRepository userRepository;
-    private MutableLiveData<User> user;
+    private MutableLiveData<User> userData;
 
     // TODO: Dependency Injection
     public EditProfileViewModel() {
@@ -29,22 +30,29 @@ public class EditProfileViewModel extends ViewModel {
             userWithNewProfile.setProfileImage(imageUrl);
 
         userRepository.updateDocument(userWithNewProfile.getUId(), userWithNewProfile);
-        user.setValue(userWithNewProfile);
+        userData.setValue(userWithNewProfile);
     }
 
     public LiveData<User> getCurrentUserData() {
-        if (user == null) {
-            user = new MutableLiveData<>();
+        if (userData == null) {
+            userData = new MutableLiveData<>();
             loadUserCurrentProfile();
         }
-        return user;
+        return userData;
     }
 
     private void loadUserCurrentProfile() {
-        user = userRepository.getDocumentByUId(authRepository.getCurrentUser().getUid(), User.class);
+        userData = userRepository.getDocumentByUId(authRepository.getCurrentUser().getUid(), User.class);
     }
 
     public boolean isCurrentUserSet() {
         return authRepository.getCurrentUser() != null;
+    }
+
+    @VisibleForTesting
+    public EditProfileViewModel(AuthRepository authRepository, UserRepository userRepository, MutableLiveData<User> userData) {
+        this.authRepository = authRepository;
+        this.userRepository = userRepository;
+        this.userData = userData;
     }
 }
