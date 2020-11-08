ce1777d5c7d12d7770931d519430ab46a60eb009
Landry <david92d92@gmail.com>
2020-11-08 07:20:33 -0500

    updating style  on SendSmsInvitationActivityTest

--- app/src/androidTest/java/com/example/bigowlapp/activity/SendSmsInvitationActivityTest.java
+++ app/src/androidTest/java/com/example/bigowlapp/activity/SendSmsInvitationActivityTest.java
@@ -1,26 +1,20 @@
 package com.example.bigowlapp.activity;
 
-import android.app.Activity;
-
-        import org.junit.Rule;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-
-        import androidx.test.espresso.action.ViewActions;
-        import androidx.test.espresso.matcher.ViewMatchers;
-        import androidx.test.ext.junit.rules.ActivityScenarioRule;
-        import androidx.test.ext.junit.runners.AndroidJUnit4;
-        import androidx.test.filters.LargeTest;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import com.example.bigowlapp.R;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static androidx.test.espresso.Espresso.onView;
-        import static androidx.test.espresso.action.ViewActions.click;
-        import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-        import static androidx.test.espresso.action.ViewActions.typeText;
-        import static androidx.test.espresso.assertion.ViewAssertions.matches;
-        import static androidx.test.espresso.matcher.ViewMatchers.withId;
-        import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -28,16 +22,13 @@ public class SendSmsInvitationActivityTest {
 
     public static final String STRING_TO_BE_TYPED = "80769";
 
-    /**
-     * Use {@link ActivityScenarioRule} to create and launch the activity under test, and close it
-     * after test completes. This is a replacement for {@link androidx.test.rule.ActivityTestRule}.
-     */
-    @Rule public ActivityScenarioRule<SendSmsInvitationActivity> activityScenarioRule
+    @Rule
+    public ActivityScenarioRule<SendSmsInvitationActivity> activityScenarioRule
             = new ActivityScenarioRule<>(SendSmsInvitationActivity.class);
 
     @Test
-    public void changeText_newActivity() {
-        // Type text and then press the button.
+    public void launchTextSmsOnButtonClick() {
+
         onView(withId(R.id.number)).perform(typeText(STRING_TO_BE_TYPED),
                 closeSoftKeyboard());
         onView(withId(R.id.send)).perform(click());
