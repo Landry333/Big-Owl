a08e36f9b39fcf7507aa46a80484a1d31e439316
Shawn <ShawnGuerraBautista@gmail.com>
2020-11-19 20:10:55 -0500

    [#30] Added functionality for getting groups and users to spinners
    Co-authored-by: RefatK <refat-17@hotmail.com>

--- app/src/main/java/com/example/bigowlapp/activity/SetSchedule.java
+++ app/src/main/java/com/example/bigowlapp/activity/SetSchedule.java
@@ -12,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bigowlapp.R;
 import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.model.User;
 import com.example.bigowlapp.viewModel.SetScheduleViewModel;
 
 import java.util.ArrayList;
@@ -19,6 +20,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SetSchedule extends AppCompatActivity {
+
+    private List<Group> listOfGroups;
+    private List<User> listOfUsers;
+
+    private Spinner groupSpinner;
+    private Spinner userSpinner;
+
+    private String title;
 
     private SetScheduleViewModel setScheduleViewModel;
 
@@ -35,40 +44,67 @@ public class SetSchedule extends AppCompatActivity {
         if (setScheduleViewModel == null) {
             setScheduleViewModel = new ViewModelProvider(this).get(SetScheduleViewModel.class);
         }
-        subscribeToData();
+        subscribeToGroupData();
     }
 
     private void initialize() {
-
+        groupSpinner = (Spinner) findViewById(R.id.select_group_spinner);
+        userSpinner = (Spinner) findViewById(R.id.select_user_spinner);
+        setUsersInSpinner();
     }
 
-    private void subscribeToData() {
+    private void subscribeToGroupData() {
         if (!setScheduleViewModel.isCurrentUserSet()) {
             return;
         }
-
-        Spinner groupSpinner = (Spinner) findViewById(R.id.select_group_spinner);
         setScheduleViewModel.getListOfGroup().observe(this, groups -> {
-            List<Group> groupSpinnerArray = new ArrayList<>(groups);
+            listOfGroups = new ArrayList<>(groups);
             List<String> groupNamesArray =
-                    groupSpinnerArray.stream().map(Group::getName).collect(Collectors.toList());
+                    listOfGroups.stream().map(Group::getName).collect(Collectors.toList());
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                     android.R.layout.simple_spinner_item, groupNamesArray);
 
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             groupSpinner.setAdapter(adapter);
-            groupSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    Log.e("position: ", Integer.toString(position));
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> parent) {
-
-                }
-            });
         });
+    }
+
+    private void setUsersInSpinner() {
+        groupSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("position: ", Integer.toString(position));
+                subscribeToUserData(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void subscribeToUserData(int position) {
+        if (!setScheduleViewModel.isCurrentUserSet()) {
+            return;
+        }
+        setScheduleViewModel.getListOfUsersFromGroup(listOfGroups.get(position))
+                .observe(this, users -> {
+                    listOfUsers = new ArrayList<>(users);
+
+                    List<String> userNamesArray =
+                            listOfUsers.stream().map(User::getFullName).collect(Collectors.toList());
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                            android.R.layout.simple_spinner_item, userNamesArray);
+
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    userSpinner.setAdapter(adapter);
+                });
+    }
+
+    private void sendSchedule() {
+
     }
 }
