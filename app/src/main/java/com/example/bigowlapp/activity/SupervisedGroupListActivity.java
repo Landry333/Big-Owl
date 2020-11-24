33af2de273689b4649b8c695d0162ddd37e1df47
KhangZhengNg (KZ) <35667311+KhangZhengNg@users.noreply.github.com>
2020-11-24 17:58:44 -0500

    #165 remove unused import

--- app/src/main/java/com/example/bigowlapp/activity/SupervisedGroupListActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/SupervisedGroupListActivity.java
@@ -2,7 +2,6 @@ package com.example.bigowlapp.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
