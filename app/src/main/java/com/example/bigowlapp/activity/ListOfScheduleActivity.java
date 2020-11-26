b8b3b654c0e3e5ff54cee54e8fbfcf0c9328e051
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2020-11-26 06:26:57 -0500

    Added links to see schedule
    - Button from Monitoring Group
    - New schedule view
    - list of schedule skeleton

---
+++ app/src/main/java/com/example/bigowlapp/activity/ListOfScheduleActivity.java
@@ -0,0 +1,41 @@
+package com.example.bigowlapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bigowlapp.R;
+import com.example.bigowlapp.viewModel.SupervisedGroupListViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ListOfScheduleActivity extends AppCompatActivity {
+    private ListView scheduleListView;
+    private ScheduleListViewModel scheduleListViewModel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_schedule);
+        initialize();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        if (scheduleListViewModel == null) {
+            scheduleListViewModel = new ViewModelProvider(this).get(ScheduleListViewModel.class);
+        }
+        subscribeToData();
+    }
+
+    private void initialize() {
+
+
+
+
+    }
+}
