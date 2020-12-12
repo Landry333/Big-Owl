036e7fc6a93143a9ace9f60beadeb351027693f0
Shawn <ShawnGuerraBautista@gmail.com>
2020-12-11 23:10:48 -0500

    [#30] Implement toolbar on current branch
    Co-authored-by: RefatK <refat-17@hotmail.com>

--- app/src/main/java/com/example/bigowlapp/activity/SetScheduleActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/SetScheduleActivity.java
@@ -7,17 +7,21 @@ import android.os.Bundle;
 import com.example.bigowlapp.R;
 import com.example.bigowlapp.fragments.ScheduleFormFragment;
 
-public class SetScheduleActivity extends AppCompatActivity {
+public class SetScheduleActivity extends BigOwlActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_schedule);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.schedule_form_container, ScheduleFormFragment.newInstance())
                     .commitNow();
         }
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_set_schedule;
     }
 }
