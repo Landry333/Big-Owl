b6e346955e3c8fd4bb02757370e1552de9c0d89e
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2021-01-08 19:07:28 -0500

    Fixed confict

--- app/src/main/java/com/example/bigowlapp/model/UserScheduleResponse.java
+++ app/src/main/java/com/example/bigowlapp/model/UserScheduleResponse.java
@@ -5,24 +5,10 @@ import com.google.firebase.Timestamp;
 public class UserScheduleResponse {
     private Response response;
     private Timestamp responseTime;
-    private boolean exists;
-
-    public UserScheduleResponse() {
-        exists = true;
-    }
 
     public UserScheduleResponse(Response response, Timestamp responseTime) {
         this.response = response;
         this.responseTime = responseTime;
-        exists = true;
-    }
-
-    public boolean isExists() {
-        return exists;
-    }
-
-    public void setExists(boolean exists) {
-        this.exists = exists;
     }
 
     public Response getResponse() {
