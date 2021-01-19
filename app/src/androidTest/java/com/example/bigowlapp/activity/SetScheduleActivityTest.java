5e5752b265ffa8833f3952238b3a23f631399bb1
Refat Kamal <refat.k17@gmail.com>
2021-01-19 16:52:05 -0500

    [#160] added test for SetScheduleActivity
    Co-authored-by: Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>

---
+++ app/src/androidTest/java/com/example/bigowlapp/activity/SetScheduleActivityTest.java
@@ -0,0 +1,43 @@
+package com.example.bigowlapp.activity;
+
+import androidx.fragment.app.Fragment;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.bigowlapp.fragments.ScheduleFormFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class SetScheduleActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<SetScheduleActivity> activityRule = new ActivityScenarioRule<>(SetScheduleActivity.class);
+
+    private SetScheduleActivity setScheduleActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        activityRule.getScenario().onActivity(activity -> {
+            setScheduleActivity = activity;
+        });
+    }
+
+    @Test
+    public void shouldGoToScheduleFormFragmentOnExecution() {
+        List<Fragment> fragmentsOnPage = setScheduleActivity.getSupportFragmentManager().getFragments();
+        // check that only one page is loaded
+        assertEquals(1, fragmentsOnPage.size());
+        Fragment currentFragment = fragmentsOnPage.get(fragmentsOnPage.size() - 1);
+        // check that the activity starts on ScheduleFormFragment page
+        assertEquals(ScheduleFormFragment.class, currentFragment.getClass());
+    }
+
+}
--- app/src/test/java/com/example/bigowlapp/fragments/ScheduleFormFragmentTest.java
+++ app/src/test/java/com/example/bigowlapp/fragments/ScheduleFormFragmentTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class ScheduleFormFragmentTest {
     @Test
     public void newInstance() {
-        // check that getInstance does indded give a ScheduleFormFragment object
+        // check that getInstance does indeed give a ScheduleFormFragment object
         assertNotNull(ScheduleFormFragment.newInstance());
     }
 }
