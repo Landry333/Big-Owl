9b73e2f2d9644cc15a2377447e69bd0ce4958936
Shawn <ShawnGuerraBautista@gmail.com>
2021-02-12 19:05:56 -0500

    [#162] updated key for uxcam

--- app/src/main/java/com/example/bigowlapp/repository/UserRepository.java
+++ app/src/main/java/com/example/bigowlapp/repository/UserRepository.java
@@ -1,11 +1,39 @@
 package com.example.bigowlapp.repository;
 
+import com.example.bigowlapp.model.LiveDataWithStatus;
 import com.example.bigowlapp.model.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class UserRepository extends Repository<User> {
 
     // TODO: Dependency Injection Implementation for Firestore
     public UserRepository() {
         super("users");
+    }
+
+    public Task<Void> isPhoneNumberUnique(String phoneNumber) {
+        LiveDataWithStatus<User> tData = new LiveDataWithStatus<>();
+        Task<QuerySnapshot> getPhoneNumberTask =
+                collectionReference.whereEqualTo("phoneNumber", phoneNumber)
+                        .limit(1)
+                        .get();
+
+        Task<Void> isSuccessfulTask = getPhoneNumberTask.continueWithTask(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot tDoc = task.getResult();
+                if (tDoc != null && !tDoc.isEmpty()) {
+                    User t = tDoc.getDocuments().get(0).toObject(User.class);
+                    tData.setSuccess(t);
+                } else {
+                    tData.setError(getDocumentNotFoundException(User.class));
+                }
+            } else {
+                tData.setError(task.getException());
+            }
+        });
+
+
+        return isSuccessfulTask;
     }
 }
--- app/src/main/java/com/example/bigowlapp/viewModel/SignUpViewModel.java
+++ app/src/main/java/com/example/bigowlapp/viewModel/SignUpViewModel.java
@@ -1,6 +1,7 @@
 package com.example.bigowlapp.viewModel;
 
 import com.example.bigowlapp.model.Group;
+import com.example.bigowlapp.model.LiveDataWithStatus;
 import com.example.bigowlapp.model.User;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
@@ -18,8 +19,15 @@ public class SignUpViewModel extends BaseViewModel {
 
     // TODO: Handle exceptions concerning the failure of the "user" database collection
     public Task<Void> createUser(String email, String password, String phoneNumber, String firstName, String lastName) {
+
+        // Check for phone number
+        // .............................................
+
+
         // add user email and password to authentication database
-        Task<AuthResult> taskAuthSignUpResult = repositoryFacade.getAuthRepository().signUpUser(email, password);
+        Task<AuthResult> taskAuthSignUpResult = repositoryFacade.getAuthRepository()
+                .signUpUser(email, password);
+
         // add basic user information to user document in firestore database
         Task<Void> taskAddUser = taskAuthSignUpResult.continueWithTask(task -> {
             if (task.isSuccessful()) {
@@ -36,6 +44,7 @@ public class SignUpViewModel extends BaseViewModel {
                 throw task.getException();
             }
         });
+
         // add a default group when the user registers to a system where the user is the supervisor
         taskAddUser.addOnSuccessListener(isSuccess -> {
             Group group = new Group();
@@ -43,6 +52,7 @@ public class SignUpViewModel extends BaseViewModel {
             group.setName(getFullName(firstName, lastName) + "'s group " + "#" + randomNumberStringGenerator());
             repositoryFacade.getGroupRepository().addDocument(group);
         });
+
         return taskAddUser;
     }
 
@@ -56,6 +66,16 @@ public class SignUpViewModel extends BaseViewModel {
         final int min = 0;
         int randomNumber = (int)((random.nextDouble() * (max - min + 1)) + min);
         return String.format(Locale.getDefault(), "%04d", randomNumber);
+    }
+
+    public boolean isPhoneNumberUnique(String phoneNumber){
+        LiveDataWithStatus<User> liveDataWithStatus =
+                repositoryFacade.getUserRepository()
+                        .getDocumentByAttribute("phoneNumber", phoneNumber, User.class);
+
+        return liveDataWithStatus.getValue() != null
+                || liveDataWithStatus.getValue().getPhoneNumber() != null;
+
     }
 
 }
