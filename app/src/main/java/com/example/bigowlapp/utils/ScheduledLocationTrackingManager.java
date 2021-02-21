6dd234326b51cd50bf455b2f40883e0b43744fd4
Refat Kamal <refat.k17@gmail.com>
2021-02-20 21:02:48 -0500

    [#177] Add addNewLocationToTrack function to track locations

--- app/src/main/java/com/example/bigowlapp/utils/ScheduledLocationTrackingManager.java
+++ app/src/main/java/com/example/bigowlapp/utils/ScheduledLocationTrackingManager.java
@@ -1,11 +1,19 @@
 package com.example.bigowlapp.utils;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
+import androidx.core.app.ActivityCompat;
+
+import com.example.bigowlapp.service.LocationBroadcastReceiver;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.GeoPoint;
 
 import java.util.UUID;
@@ -20,13 +28,29 @@ public class ScheduledLocationTrackingManager {
 
     private Context context;
     private GeofencingClient geofencingClient;
+    private PendingIntent geofencePendingIntent;
 
     public ScheduledLocationTrackingManager(Context context) {
         this.context = context;
         geofencingClient = LocationServices.getGeofencingClient(context);
     }
 
-    public GeofencingRequest buildRequestToTrack(GeoPoint locationCoords) {
+    public Task<Void> addNewLocationToTrack(GeoPoint locationCoords) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Handle missing permissions case
+            // Consider calling ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+
+        return geofencingClient.addGeofences(buildRequestToTrack(locationCoords), getGeofencePendingIntent());
+    }
+
+    private GeofencingRequest buildRequestToTrack(GeoPoint locationCoords) {
         return new GeofencingRequest.Builder()
                 .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
                 .addGeofence(this.buildLocationToTrack(locationCoords))
@@ -43,5 +67,16 @@ public class ScheduledLocationTrackingManager {
                 .setNotificationResponsiveness(NO_NOTIFY_DELAY)
                 .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
                 .build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+
+        Intent intent = new Intent(context, LocationBroadcastReceiver.class);
+        geofencePendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
     }
 }
