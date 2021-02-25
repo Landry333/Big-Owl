093a2be83c0509861ffeeca0db290bf758a59ded
Shawn <ShawnGuerraBautista@gmail.com>
2021-02-24 19:43:41 -0500

    [#179] Created AlarmBroadcastReceiver

---
+++ app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.example.bigowlapp.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
