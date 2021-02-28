19f06a98ae0ddd6be8ffb26a61990bcd556ec07b
Shawn <ShawnGuerraBautista@gmail.com>
2021-02-27 20:56:02 -0500

    [#179] Create a schedule from an intent created from the AlarmManager when Alarm is activated

--- app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.example.bigowlapp.model.Schedule;
+import com.google.firebase.firestore.GeoPoint;
+
 /**
  * The purpose of this BroadcastReceiver is to execute code after the time activation of an alarm.
  * The alarms are set/defined in
@@ -11,14 +14,25 @@ import android.content.Intent;
  */
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
+    private final String EXTRA_UID = "Uid";
+    private final String EXTRA_LATITUDE = "Latitude";
+    private final String EXTRA_LONGITUDE = "Longitude";
+    private final double DEFAULT_VALUE = 0.0;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Schedule activatedSchedule = getSchedule(intent);
         // Run the location/Geofencing code
-        // Intent should have
-        /*
-          intent.putExtra("Uid", schedule.getUid());
-          intent.putExtra("Latitude", schedule.getLocation().getLatitude());
-          intent.putExtra("Longitude", schedule.getLocation().getLongitude());
-         */
+        // activatedSchedule should only have UID, LONGITUDE, LATITUDE
     }
+
+    private Schedule getSchedule(Intent intent) {
+        Schedule schedule = new Schedule();
+        schedule.setUid(intent.getStringExtra(EXTRA_UID));
+        GeoPoint geoPoint = new GeoPoint(intent.getDoubleExtra(EXTRA_LATITUDE, DEFAULT_VALUE),
+                intent.getDoubleExtra(EXTRA_LONGITUDE, DEFAULT_VALUE));
+        schedule.setLocation(geoPoint);
+        return schedule;
+    }
+
 }
