e8d4b2b0d8a1d6abb9708b5f0288e8f4ab882f27
Shawn <ShawnGuerraBautista@gmail.com>
2021-02-27 23:59:03 -0500

    [#179] Quality: Eliminated minor code smell; comply with java language spec

--- app/src/main/java/com/example/bigowlapp/utils/IntentConstants.java
+++ app/src/main/java/com/example/bigowlapp/utils/IntentConstants.java
@@ -6,7 +6,7 @@ public final class IntentConstants {
         // Empty private constructor because an object of this class shouldn't be instantiated
     }
 
-    public final static String EXTRA_UID = "Uid";
-    public final static String EXTRA_LATITUDE = "Latitude";
-    public final static String EXTRA_LONGITUDE = "Longitude";
+    public static final String EXTRA_UID = "Uid";
+    public static final String EXTRA_LATITUDE = "Latitude";
+    public static final String EXTRA_LONGITUDE = "Longitude";
 }
