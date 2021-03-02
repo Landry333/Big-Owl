d4b893ad3e2027fb45194deb70050435893b487b
Shawn <ShawnGuerraBautista@gmail.com>
2021-03-02 18:40:24 -0500

    [#179] Changed default message for exception handling in PhoneNumberTakenException

--- app/src/main/java/com/example/bigowlapp/repository/exception/PhoneNumberTakenException.java
+++ app/src/main/java/com/example/bigowlapp/repository/exception/PhoneNumberTakenException.java
@@ -2,7 +2,7 @@ package com.example.bigowlapp.repository.exception;
 
 public class PhoneNumberTakenException extends Exception {
 
-    private static final String DEFAULT_MESSAGE = "Phone Number is already in the database.";
+    private static final String DEFAULT_MESSAGE = "This phone number is already associated to an account";
 
     public PhoneNumberTakenException() {
         super(DEFAULT_MESSAGE);
