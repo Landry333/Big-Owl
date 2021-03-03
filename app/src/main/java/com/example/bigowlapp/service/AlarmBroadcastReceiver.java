1c9fba96747c1286131de003fc5a21f8b00a5c30
Landry <david92d92@gmail.com>
2021-03-03 03:58:48 -0500

    #15 adding temporary version of AlarmBroadcastReceiver

--- app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
@@ -4,34 +4,66 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.example.bigowlapp.model.Schedule;
-import com.google.firebase.firestore.GeoPoint;
+import androidx.lifecycle.LifecycleOwner;
 
-import static com.example.bigowlapp.utils.IntentConstants.EXTRA_LATITUDE;
-import static com.example.bigowlapp.utils.IntentConstants.EXTRA_LONGITUDE;
+import com.example.bigowlapp.model.LiveDataWithStatus;
+import com.example.bigowlapp.model.Schedule;
+import com.example.bigowlapp.model.User;
+import com.example.bigowlapp.repository.RepositoryFacade;
+import com.example.bigowlapp.utils.SmsSender;
+import com.example.bigowlapp.utils.SupervisorSchedulesAlarmManager;
+
 import static com.example.bigowlapp.utils.IntentConstants.EXTRA_UID;
 
 /**
  * The purpose of this BroadcastReceiver is to execute code after the time activation of an alarm.
  * The alarms are set/defined in
- * {@link com.example.bigowlapp.utils.AlarmBroadcastReceiverManager}
+ * {@link SupervisorSchedulesAlarmManager}
  */
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
+
+    RepositoryFacade repositoryFacade = RepositoryFacade.getInstance();
+    SmsSender smsSender;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // Execute Schedule activatedSchedule = getSchedule(intent); to get the schedule
         // Run the location/Geofencing code
         // activatedSchedule should only have UID, LONGITUDE, LATITUDE
+        Schedule activatedSchedule = getSchedule(intent);
+        if (activatedSchedule.getLocation().equals(null)) {
+            LiveDataWithStatus<Schedule> supervisorScheduleData = repositoryFacade.getScheduleRepository()
+                    .getDocumentByUid(activatedSchedule.getUid(), Schedule.class);
+            supervisorScheduleData.observe((LifecycleOwner) this, supervisorSchedule -> {
+                if (supervisorSchedule == null) {
+                    return;
+                } else {
+                    for (String scheduleMemberUid : supervisorSchedule.getMemberList()) {
+                        if (supervisorSchedule.getUserScheduleResponseMap().get(scheduleMemberUid)
+                                .getAttendance.getScheduleLocated() == NOT_DETECTED) {
+                            String scheduleMemberPhoneNumber = repositoryFacade.getUserRepository()
+                                    .getDocumentByUid(scheduleMemberUid, User.class)
+                                    .getValue().getPhoneNumber();
+                            smsSender.sendSMS(context, scheduleMemberPhoneNumber,
+                                    "ATTENTION: This is a message from BigOwl attendance system." +
+                                            "BigOwl wasn't able to get your verify your current location for" +
+                                            " your next schedule. Please make sure your internet and the " +
+                                            "BigOwl mobile application are turned on");
+                        }
+                    }
+                }
+        });
     }
+
+}
 
     private Schedule getSchedule(Intent intent) {
         double defaultValueLatLng = 0.0;
         Schedule schedule = new Schedule();
         schedule.setUid(intent.getStringExtra(EXTRA_UID));
-        GeoPoint geoPoint = new GeoPoint(intent.getDoubleExtra(EXTRA_LATITUDE, defaultValueLatLng),
+        /*GeoPoint geoPoint = new GeoPoint(intent.getDoubleExtra(EXTRA_LATITUDE, defaultValueLatLng),
                 intent.getDoubleExtra(EXTRA_LONGITUDE, defaultValueLatLng));
-        schedule.setLocation(geoPoint);
+        schedule.setLocation(geoPoint);*/
         return schedule;
     }
 
