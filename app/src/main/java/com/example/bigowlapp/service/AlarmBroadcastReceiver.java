1038cd82193063b2b493b607d07de40e93847b2c
Shawn <ShawnGuerraBautista@gmail.com>
2021-03-05 16:46:23 -0500

    [#179] Notifications should be able to wake up the device

--- app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/AlarmBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.PowerManager;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -34,6 +35,11 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         // activatedSchedule should only have UID, LONGITUDE, LATITUDE
         // TODO: --START-- REMOVE BEFORE MERGING PULL REQUEST. THIS IS ONLY FOR TESTING PURPOSES.
         createNotificationChannel(context);
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (!pm.isInteractive()) {
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "myApp:notificationLock");
+            wl.acquire(3000);
+        }
         Schedule schedule = getSchedule(intent);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notification)
