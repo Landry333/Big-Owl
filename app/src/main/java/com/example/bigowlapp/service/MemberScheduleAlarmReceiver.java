e45d7c7378257c1d08cead5915b79a7b1321301f
Refat Kamal <refat.k17@gmail.com>
2021-03-09 00:40:53 -0500

    [#177] Quality: remove unused import

--- app/src/main/java/com/example/bigowlapp/service/MemberScheduleAlarmReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/MemberScheduleAlarmReceiver.java
@@ -3,7 +3,6 @@ package com.example.bigowlapp.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.example.bigowlapp.model.Schedule;
 import com.example.bigowlapp.utils.MemberScheduleAlarmManager;
