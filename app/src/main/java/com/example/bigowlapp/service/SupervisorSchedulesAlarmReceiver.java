d32994e493fbce93c6b73ed6e58584f67f974cbb
Landry <david92d92@gmail.com>
2021-03-12 09:24:51 -0500

    #15 modifying the intent attribute, fixing code for data subscribtion and reducing sms message size

--- app/src/main/java/com/example/bigowlapp/service/SupervisorSchedulesAlarmReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/SupervisorSchedulesAlarmReceiver.java
@@ -6,11 +6,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.lifecycle.LifecycleOwner;
-
-import com.example.bigowlapp.activity.LoginPageActivity;
 import com.example.bigowlapp.model.Attendance;
-import com.example.bigowlapp.model.LiveDataWithStatus;
 import com.example.bigowlapp.model.Schedule;
 import com.example.bigowlapp.model.User;
 import com.example.bigowlapp.repository.RepositoryFacade;
@@ -27,7 +23,7 @@ import static com.example.bigowlapp.utils.IntentConstants.EXTRA_UID;
 public class SupervisorSchedulesAlarmReceiver extends BroadcastReceiver {
 
     RepositoryFacade repositoryFacade = RepositoryFacade.getInstance();
-    SmsSender smsSender;
+    SmsSender smsSender = new SmsSender();
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -35,29 +31,37 @@ public class SupervisorSchedulesAlarmReceiver extends BroadcastReceiver {
         // Run the location/Geofencing code
         // activatedSchedule should only have UID, LONGITUDE, LATITUDE
         Toast.makeText(context, "Received Alarm and started to send sms", Toast.LENGTH_LONG).show();
-        Schedule activatedSchedule = getSchedule(intent);
-        Log.e("activatedSchedule", activatedSchedule.getStartTime().toString());
-        LiveDataWithStatus<Schedule> supervisorScheduleData = repositoryFacade.getScheduleRepository()
-                .getDocumentByUid(activatedSchedule.getUid(), Schedule.class);
-        supervisorScheduleData.observeForever(supervisorSchedule -> {
-            if (supervisorSchedule == null) {
-                return;
-            } else {
-                for (String scheduleMemberUid : supervisorSchedule.getMemberList()) {
-                    if (supervisorSchedule.getUserScheduleResponseMap().get(scheduleMemberUid)
-                            .getAttendance().getScheduleLocated() == Attendance.LocatedStatus.NOT_DETECTED) {
-                        String scheduleMemberPhoneNumber = repositoryFacade.getUserRepository()
-                                .getDocumentByUid(scheduleMemberUid, User.class)
-                                .getValue().getPhoneNumber();
-                        smsSender.sendSMS(context, scheduleMemberPhoneNumber,
-                                "ATTENTION: This is a message from BigOwl attendance system." +
-                                        "BigOwl wasn't able to get your current location for" +
-                                        " your next schedule. Please make sure your internet and the " +
-                                        "BigOwl mobile application are turned on");
+        String scheduleId = intent.getStringExtra("scheduleUid");
+        //Toast.makeText(context, "scheduleId from intent: "+scheduleId, Toast.LENGTH_LONG).show();
+        //String scheduleID = getIntent().getStringExtra(Intent.EXTRA_UID);
+        //Schedule activatedSchedule = getSchedule(intent);
+        Log.e("scheduleId from intent", scheduleId);
+
+        repositoryFacade.getScheduleRepository().getDocumentByUid(scheduleId, Schedule.class)
+                .observeForever(supervisorSchedule -> {
+                    if (supervisorSchedule == null) {
+                        return;
+                    } else {
+                        for (String scheduleMemberUid : supervisorSchedule.getMemberList()) {
+                            if (supervisorSchedule.getUserScheduleResponseMap().get(scheduleMemberUid)
+                                    .getAttendance().getScheduleLocated() == Attendance.LocatedStatus.NOT_DETECTED) {
+                                repositoryFacade.getUserRepository()
+                                        .getDocumentByUid(scheduleMemberUid, User.class)
+                                        .observeForever(scheduleMember -> {
+                                            Log.e("scheduleMember", scheduleMember.getFirstName());
+                                            Log.e("scheduleMemberPhone", scheduleMember.getPhoneNumber());
+                                            String scheduleMemberPhoneNum = scheduleMember.getPhoneNumber();
+                                            smsSender.sendSMS(context, scheduleMemberPhoneNum,
+                                                    "ATTENTION: BigOwl wasn't able to get your current location for" +
+                                                            " your next attendance. Please make sure your internet and " +
+                                                            "BigOwl app are on");
+
+                                        });
+
+                            }
+                        }
                     }
-                }
-            }
-        });
+                });
 
     }
 
