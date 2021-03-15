f5e9c5f39c552923d42621dcd67e6b144de032b8
Landry <david92d92@gmail.com>
2021-03-15 15:14:46 -0400

    #12 creating a class to send sms

---
+++ app/src/main/java/com/example/bigowlapp/utils/SmsSender.java
@@ -0,0 +1,22 @@
+package com.example.bigowlapp.utils;
+
+import android.content.Context;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+public class SmsSender {
+
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+
+    public SmsSender() {
+
+    }
+
+    public void sendSMS(Context context, String smsNumber, String smsMessage) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(smsNumber, null, smsMessage, null, null);
+        Toast.makeText(context, "SMS SENT FROM SMS SENDER OBJECT", Toast.LENGTH_SHORT).show();
+    }
+
+}
