8d0c1cba54af789230410b3b7f1d7be102fdd7d7
Landry <david92d92@gmail.com>
2021-03-16 15:09:04 -0400

    #15 setting the delay time to send text sms to 4 min and cleaning code

--- app/src/main/java/com/example/bigowlapp/utils/SupervisorSchedulesAlarmManager.java
+++ app/src/main/java/com/example/bigowlapp/utils/SupervisorSchedulesAlarmManager.java
@@ -4,31 +4,21 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.example.bigowlapp.activity.LoginPageActivity;
-import com.example.bigowlapp.model.Response;
 import com.example.bigowlapp.model.Schedule;
 import com.example.bigowlapp.repository.ScheduleRepository;
 import com.example.bigowlapp.service.SupervisorSchedulesAlarmReceiver;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.example.bigowlapp.utils.IntentConstants.EXTRA_UID;
-
 /**
- * The purpose of this class is to set/define alarms for member schedules that the app will set.
+ * The purpose of this class is to set/define alarms for supervisor schedules that the app will set.
  * After the time activation of an alarm, code will be executed from
  * {@link SupervisorSchedulesAlarmReceiver}
  */
@@ -55,92 +45,41 @@ public class SupervisorSchedulesAlarmManager {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         getSchedulesForSupervisor(userID)
                 .addOnSuccessListener(scheduleList -> {
-                    Toast.makeText(context, "Found supervisor schedules and started setAlarms", Toast.LENGTH_LONG).show();
                     for (Schedule schedule : scheduleList) {
-                        Log.e("found schedule", schedule.toString());
-
                         Intent intent = new Intent(context, SupervisorSchedulesAlarmReceiver.class);
-
                         intent.putExtra("scheduleUid", schedule.getUid());
-                        Log.e("ScheduleId in Intent", schedule.getUid());
-                        Log.e("supervisor Intent", intent.toString());
-                        //intent.putExtra(EXTRA_LATITUDE, schedule.getLocation().getLatitude());
-                        //intent.putExtra(EXTRA_LONGITUDE, schedule.getLocation().getLongitude());
                         PendingIntent pendingIntent = PendingIntent
                                 .getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                         alarmManager.set(AlarmManager.RTC_WAKEUP,
-                                addMinutesToDate(schedule.getStartTime().toDate(),12).getTime(), pendingIntent);
-                        Log.e("supervisor pendingIntent", pendingIntent.toString());
+                                addMinutesToDate(schedule.getStartTime().toDate(), 4).getTime(), pendingIntent);
                     }
-                }).addOnFailureListener(error -> {
-                    Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
-                    Log.e("addOnFailureListener Error", error.getMessage());
-        });
+                });
     }
+
     public Date addMinutesToDate(Date date, int minutes) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.MINUTE, minutes);
         return calendar.getTime();
     }
+
     /**
      * Obtains the list of schedules that the user is supervising, and filters in schedules that have been accepted
      *
-     * @param userID The Id of the user
-     * @return A Task that contains a list of schedule for the user that hasn't been attended
+     * @param userID The Id of the supervisor
+     * @return A Task that contains a list of schedules for the supervisor that haven't yet passed
      */
 
     private Task<List<Schedule>> getSchedulesForSupervisor(String userID) {
-        return scheduleRepository.getTaskListSchedulesForSupervisor(userID).addOnFailureListener(error -> {
-            Log.e("addOnFailureListener Error1", error.getMessage());})
+        return scheduleRepository.getTaskListSchedulesForSupervisor(userID)
                 .onSuccessTask(tDocs -> {
                     List<Schedule> scheduleList = tDocs.toObjects(Schedule.class);
                     List<Schedule> acceptedScheduleList = scheduleList.stream()
                             //.filter(schedule -> schedule.getUserScheduleResponseMap().get(userID).getResponse() == Response.ACCEPT)
                             .collect(Collectors.toList());
                     return Tasks.forResult(acceptedScheduleList);
-                }).addOnFailureListener(error -> {
-                            Log.e("addOnFailureListener Error2", error.getMessage());});
-
-    }
-
-    public Task<QuerySnapshot> getTaskListSchedulesForSupervisor(String userID) {
-        return db.collection("schedules").whereEqualTo("groupSupervisorUid", userID)
-                .whereGreaterThanOrEqualTo("startTime", Timestamp.now())
-                .orderBy("startTime", Query.Direction.ASCENDING)
-                .get();
-    }
-
-    private Task<List<Schedule>> getSchedulesForUser(String userID) {
-        return scheduleRepository.getTaskListSchedulesForUser(userID)
-                .onSuccessTask(tDocs -> {
-                    List<Schedule> scheduleList = tDocs.toObjects(Schedule.class);
-                    List<Schedule> acceptedScheduleList = scheduleList.stream()
-                            .filter(schedule -> schedule.getUserScheduleResponseMap().get(userID).getResponse() == Response.ACCEPT)
-                            .collect(Collectors.toList());
-                    return Tasks.forResult(acceptedScheduleList);
                 });
+
     }
 
-    /*private Task<List<Schedule>> getSchedulesForSupervisor(String userID) {
-        return scheduleRepository.getTaskListSchedulesForSupervisor(userID)
-                .continueWithTask(task -> {
-                    if (task.isSuccessful()) {
-                        QuerySnapshot tDocs = task.getResult();
-                        List<Schedule> scheduleList = tDocs.toObjects(Schedule.class);
-                        List<Schedule> acceptedScheduleList = new ArrayList<>();
-                        for (Schedule schedule : scheduleList) {
-                            if (schedule.getGroupSupervisorUid().equalsIgnoreCase(userID)) {
-                            //if (schedule.getUserScheduleResponseMap()
-                                    .get(userID).getResponse() == Response.ACCEPT) {//
-                                acceptedScheduleList.add(schedule);
-                            }
-                        }
-                        return Tasks.forResult(acceptedScheduleList);
-                    } else {
-                        throw task.getException();
-                    }
-
-                });
-    }*/
 }
