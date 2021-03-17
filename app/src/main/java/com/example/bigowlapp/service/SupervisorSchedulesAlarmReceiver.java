c7142166a64a4dc13be115045db4ef01ced075ee
Landry <david92d92@gmail.com>
2021-03-16 21:18:41 -0400

    #15 cleaning code

--- app/src/main/java/com/example/bigowlapp/service/SupervisorSchedulesAlarmReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/SupervisorSchedulesAlarmReceiver.java
@@ -3,6 +3,7 @@ package com.example.bigowlapp.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.example.bigowlapp.model.Attendance;
 import com.example.bigowlapp.model.Schedule;
