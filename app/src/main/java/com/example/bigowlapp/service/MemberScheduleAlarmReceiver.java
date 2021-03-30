5a7c71c6c33c930cf27aa6abea461963e60f6f7d
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2021-03-30 05:53:13 -0400

    Update MemberScheduleAlarmReceiver.java

--- app/src/main/java/com/example/bigowlapp/service/MemberScheduleAlarmReceiver.java
+++ app/src/main/java/com/example/bigowlapp/service/MemberScheduleAlarmReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.bigowlapp.model.Schedule;
 import com.example.bigowlapp.utils.MemberScheduleAlarmManager;
