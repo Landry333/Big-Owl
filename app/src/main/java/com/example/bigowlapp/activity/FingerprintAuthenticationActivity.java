b2de282b6313fbc54f09a8016a44326d070ae0d4
Landry <david92d92@gmail.com>
2021-03-31 17:05:34 -0400

    #13 adding more meaniningfull text in toast

--- app/src/main/java/com/example/bigowlapp/activity/FingerprintAuthenticationActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/FingerprintAuthenticationActivity.java
@@ -38,14 +38,14 @@ public class FingerprintAuthenticationActivity extends AppCompatActivity {
     private Button btnFingerprintAuthMaybeLater;
     private HomePageViewModel homePageViewModel;
     private static final String PROPOSE_FINGERPRINT_AUTH = "Add fingerprint authentication to secure your account?\n\n" +
-            "NOTE: This will also make your account ONLY accessible by a device having sim card 1 phone number same " +
+            "NOTE: This will also make your account ONLY accessible by a device having sim card_1 phone number same " +
             "as the phone number you saved in your account";
     private static final String IS_FINGERPRINT_AUTH_REGISTERED = "You are registered to fingerprint authentication.\n\n" +
             "You can modify your choice on Edit profile";
     private static final String NO_COMPATIBILITY_WITH_PHONE = "Sorry, this additional security service is not available with" +
             " this phone or with your telephony provider\n\nPhone number on this phone should be the same as in your account";
     private static final String NOT_ALLOWED = "You are not allowed to access this account\n\n" +
-            "Sim card 1 phone number on this phone should be the same as in your account";
+            "Sim card_1 phone number on this phone should be the same as in your account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,7 +104,7 @@ public class FingerprintAuthenticationActivity extends AppCompatActivity {
         });
         btnFingerprintAuthMaybeLater.setOnClickListener(v -> {
             goToHomePage();
-            Toast.makeText(this, "You are logged in", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "You are logged in. Edit profile to change your choice", Toast.LENGTH_LONG).show();
         });
         btnGoToHomePage.setOnClickListener(v -> goToHomePage());
 
