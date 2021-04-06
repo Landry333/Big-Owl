ab5db84e116c0f8438b52406a2762f133f32cfe7
Refat Kamal <refat.k17@gmail.com>
2021-04-05 20:28:40 -0400

    [#176] use new uxcam key

--- app/src/main/java/com/example/bigowlapp/activity/WelcomePageActivity.java
+++ app/src/main/java/com/example/bigowlapp/activity/WelcomePageActivity.java
@@ -17,7 +17,7 @@ public class WelcomePageActivity extends AppCompatActivity implements Constants 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        UXCam.startWithKey("0y1acgugqvq0xgc");
+        UXCam.startWithKey("u5kc5hd96a2m2vm");
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             Intent i = new Intent(WelcomePageActivity.this, LoginPageActivity.class);
             startActivity(i);
