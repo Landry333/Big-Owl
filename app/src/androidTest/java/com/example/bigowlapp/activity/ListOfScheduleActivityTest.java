59e4d42651c82fff88aa1a8898a0ef6c1446bf61
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2021-04-06 01:16:38 -0400

    Create ListOfScheduleActivityTest.java

---
+++ app/src/androidTest/java/com/example/bigowlapp/activity/ListOfScheduleActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.bigowlapp.activity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListOfScheduleActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void getContentView() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void setSupervisedGroupListViewModel() {
+    }
+}
