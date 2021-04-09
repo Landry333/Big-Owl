500972b262f8f4b4afda0d5ceb3b623382fb5898
Refat Kamal <refat.k17@gmail.com>
2021-04-08 23:37:15 -0400

    [#289] try to fix ci by providing contacts permissions

--- app/src/androidTest/java/com/example/bigowlapp/activity/SearchContactsToSuperviseTest.java
+++ app/src/androidTest/java/com/example/bigowlapp/activity/SearchContactsToSuperviseTest.java
@@ -1,11 +1,18 @@
 package com.example.bigowlapp.activity;
 
+import android.Manifest;
+
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
 
 import com.example.bigowlapp.R;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.replaceText;
@@ -13,10 +20,15 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class SearchContactsToSuperviseTest {
 
-    private ActivityScenario<SearchContactsToSupervise> scenario;
+    @Rule
+    public final GrantPermissionRule permissionRule = GrantPermissionRule.grant(
+            Manifest.permission.READ_CONTACTS);
 
+    private ActivityScenario<SearchContactsToSupervise> scenario;
 
     @Before
     public void setUp() throws Exception {
