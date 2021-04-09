add1313ec17a097e47f30ab745a9e4025aa0ffeb
KZ <ng_khang_zheng@hotmail.com>
2021-04-09 18:31:31 -0400

    #25 add empty schedule test to hit 100% coverage!

--- app/src/androidTest/java/com/example/bigowlapp/activity/ScheduleReportActivityTest.java
+++ app/src/androidTest/java/com/example/bigowlapp/activity/ScheduleReportActivityTest.java
@@ -49,7 +49,10 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.atMostOnce;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -123,8 +126,7 @@ public class ScheduleReportActivityTest {
         testScheduleMembersMap.put("testMember005", new UserScheduleResponse(Response.NEUTRAL, null));
         testSchedule.setUserScheduleResponseMap(testScheduleMembersMap);
         testSchedule.setLocation(new GeoPoint(45.49661075, -73.57853574999999));
-        testScheduleData = new MutableLiveData<>(testSchedule);
-        testScheduleData.postValue(testSchedule);
+        testScheduleData = new MutableLiveData<>();
 
         when(mockScheduleReportViewModel.isCurrentUserSupervisor(testSupervisor.getUid())).thenReturn(true);
         when(mockScheduleReportViewModel.getCurrentScheduleData(testSchedule.getUid())).thenReturn(testScheduleData);
@@ -147,6 +149,7 @@ public class ScheduleReportActivityTest {
 
     @Test
     public void displayTest() {
+        testScheduleData.postValue(testSchedule);
         onView(withId(R.id.top_app_bar)).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.schedule_report_title), withText(testSchedule.getTitle()))).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.schedule_report_start_time), withText(String.valueOf(testSchedule.getStartTime().toDate())))).check(matches(isDisplayed()));
@@ -161,6 +164,18 @@ public class ScheduleReportActivityTest {
     }
 
     @Test
+    public void emptyScheduleTest() {
+        testScheduleData.postValue(null);
+
+        onView(allOf(withId(R.id.schedule_report_title), withText("schedule_report_title"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.schedule_report_start_time), withText("schedule_report_start_time"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.schedule_report_end_time), withText("schedule_report_end_time"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.schedule_report_location), withText("schedule_report_location"))).check(matches(isDisplayed()));
+        verify(mockScheduleReportViewModel, atMost(0)).getScheduleMemberNameMap(anyList());
+        assertNull(currentActivity.getScheduleReportMembersAdapter());
+    }
+
+    @Test
     public void scheduleScheduledInOneHourTest() {
         testSchedule.setStartTime(new Timestamp(timestampNow.getSeconds() + 3600, 0));
         testSchedule.setEndTime(new Timestamp(timestampNow.getSeconds() + 7200, 0));
@@ -171,7 +186,7 @@ public class ScheduleReportActivityTest {
 
     @Test
     public void scheduleOnGoingTest() {
-        testSchedule.setStartTime(new Timestamp(timestampNow.getSeconds() - 3600, 0));
+        testSchedule.setStartTime(new Timestamp(timestampNow.getSeconds() - 900, 0));
         testSchedule.setEndTime(new Timestamp(timestampNow.getSeconds() + 3600, 0));
         testSchedule.getUserScheduleResponseMap().put("testMember000",
                 new UserScheduleResponse(Response.ACCEPT,
