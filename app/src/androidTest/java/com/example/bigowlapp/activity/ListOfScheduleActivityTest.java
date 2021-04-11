f1606c3f7204b03ebbac0b584d3d82dc86926a6a
Kennan Hoa <46039521+KennanHoa@users.noreply.github.com>
2021-04-11 00:22:15 -0400

    Update ListOfScheduleActivityTest.java
    - Clean up

--- app/src/androidTest/java/com/example/bigowlapp/activity/ListOfScheduleActivityTest.java
+++ app/src/androidTest/java/com/example/bigowlapp/activity/ListOfScheduleActivityTest.java
@@ -1,11 +1,8 @@
 package com.example.bigowlapp.activity;
 
 import android.content.Intent;
-import android.os.SystemClock;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.MutableLiveData;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -13,25 +10,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.example.bigowlapp.R;
-import com.example.bigowlapp.model.Group;
 import com.example.bigowlapp.model.LiveDataWithStatus;
 import com.example.bigowlapp.model.Schedule;
 import com.example.bigowlapp.model.User;
-import com.example.bigowlapp.repository.AuthRepository;
-import com.example.bigowlapp.repository.GroupRepository;
-import com.example.bigowlapp.view_model.ScheduleListViewModel;
 import com.example.bigowlapp.view_model.ScheduleListViewModel;
 import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseUser;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -44,14 +34,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -67,9 +52,7 @@ public class ListOfScheduleActivityTest {
     private List<Schedule> testUserScheduleList;
     private LiveDataWithStatus<List<Schedule>> testScheduleListData;
     private ListOfScheduleActivity currentActivity;
-
     private ActivityScenario<ListOfScheduleActivity> activityScenario;
-    private User testGroupSupervisor;
 
     @Before
     public void setUp() throws Exception {
@@ -88,31 +71,29 @@ public class ListOfScheduleActivityTest {
         testUserScheduleList = Collections.singletonList(Schedule.getPrototypeSchedule());
         testScheduleListData = new LiveDataWithStatus<>(testUserScheduleList);
         when(ScheduleListViewModel.getScheduleList(eq(false), any())).thenReturn(testScheduleListData);
+        when(ScheduleListViewModel.getScheduleList(eq(true), any())).thenReturn(testScheduleListData);
 
-        activityScenario = ActivityScenario.launch(testArrivingIntent);
-
-        activityScenario.moveToState(Lifecycle.State.CREATED);
-        activityScenario.onActivity(activity -> {
-            currentActivity = activity;
-            activity.setScheduleListViewModel(ScheduleListViewModel);
-        });
-        activityScenario.moveToState(Lifecycle.State.RESUMED);
+        startActivityScenario(testArrivingIntent);
     }
 
     @Test
     public void noScheduleTest() {
+        startActivityScenario(testArrivingIntent);
         testScheduleListData.postValue(null);
         onView(withText("No schedule found!")).check(matches(isDisplayed()));
     }
 
     @Test
     public void viewScheduleTest() {
+        startActivityScenario(testArrivingIntent);
         testScheduleListData.postValue(getScheduleList());
         onView(withText("sched3")).check(matches(isDisplayed()));
     }
 
     @Test
     public void clickOnScheduleTestAsSupervisor() {
+        testArrivingIntent.putExtra("isUserTheGroupSupervisor", true);
+        startActivityScenario(testArrivingIntent);
         testScheduleListData.postValue(getScheduleList());
         Intent getIntentToSchedule = currentActivity.getIntentToScheduleForTest();
         assertNull(getIntentToSchedule);
@@ -129,6 +110,7 @@ public class ListOfScheduleActivityTest {
 
     @Test
     public void clickOnScheduleTestAsMonitoredUser() {
+        startActivityScenario(testArrivingIntent);
         testScheduleListData.postValue(getScheduleList());
         Intent getIntentToSchedule = currentActivity.getIntentToScheduleForTest();
         assertNull(getIntentToSchedule);
@@ -148,17 +130,27 @@ public class ListOfScheduleActivityTest {
         assertEquals("testArrivingIntentSupervisorName", getIntentToSchedule.getStringExtra("supervisorName"));
     }
 
-
     private List<Schedule> getScheduleList() {
         List<Schedule> scheduleList = new ArrayList<>();
 
-        for (int i = 0; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             Schedule newSchedule = new Schedule();
             newSchedule.setStartTime(Timestamp.now());
-            newSchedule.setTitle("sched"+i);
-            newSchedule.setGroupSupervisorUid("supervisor"+i);
+            newSchedule.setTitle("sched" + i);
+            newSchedule.setGroupSupervisorUid("supervisor" + i);
             scheduleList.add(newSchedule);
         }
         return scheduleList;
+    }
+
+    private void startActivityScenario(Intent intent) {
+        activityScenario = ActivityScenario.launch(intent);
+
+        activityScenario.moveToState(Lifecycle.State.CREATED);
+        activityScenario.onActivity(activity -> {
+            currentActivity = activity;
+            activity.setScheduleListViewModel(ScheduleListViewModel);
+        });
+        activityScenario.moveToState(Lifecycle.State.RESUMED);
     }
 }
